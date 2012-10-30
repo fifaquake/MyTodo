d3750a5aef5530a89fba33e7cff0a6d9f4c63c79
peng_laptop <lipengweb@gmail.com>
2012-10-30 09:30:20 +0800

    Implement the reg page.

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,11 @@
 exports.index = function(req, res){
   res.render('index', {title: '主页'});
 };
+
+exports.reg = function(req, res) {
+  res.render('register', {title: '注册'});
+}
+
+exports.doReg = function(req, res) {
+  res.render('index', {title : '主页'});
+}

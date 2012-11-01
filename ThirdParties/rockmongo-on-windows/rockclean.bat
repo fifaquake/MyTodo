efc5d78eb81640f0eddae941090782ee27c8326c
peng_laptop <lipengweb@gmail.com>
2012-11-01 16:35:55 +0800

    Delete rockmongo.

--- ThirdParties/rockmongo-on-windows/rockclean.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-del nginx\logs\access.log
-del nginx\logs\error.log
-del nginx\logs\nginx.pid
--- ThirdParties/rockmongo-on-windows/rockstop.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-taskkill /f /IM nginx.exe
-taskkill /f /IM php-cgi.exe
-
-EXIT

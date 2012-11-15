d35f7fcf94bc3e9c77ae03b884801a95f579e9c5
Peng_Laptop <lipengweb@gmail.com>
2012-11-15 08:12:07 +0800

    Finished the function to use AJAX during deleting an item.

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,12 @@
 /*
  * GET home page.
  */
+url = require('url');
+
 var crypto = require('crypto');
 var User = require('../models/user.js');
 var TodoItem = require('../models/todoItem.js');
+
 
 exports.index = function (req, res) {
   res.render('index', { title: '主页' });
@@ -16,7 +19,11 @@ exports.deleteTodoItem = function (req, res) {
   var curUser = new User(res.locals.user);
   curUser.deleteTodoItem(req.params.id, function (err, r) {
     req.session.priority = "all";
-    res.redirect('/content');
+    var queryObj = url.parse(req.url, true).query;
+
+    // We can send back some element to client, and the user can modify the UI according to the responsed data.
+    // Here we have no need to send the data for deleting item.
+    res.send(queryObj.callback + '(\'{"message": "test"}\')');
   });
 }
 
